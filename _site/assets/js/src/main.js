70531aa888de164fe85234c792a411ef060ae80f
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-08-13 20:55:21 +0100

    cleaning post and post header with first version of responsive navigation

--- _site/assets/js/src/main.js
+++ _site/assets/js/src/main.js
@@ -1,35 +1,116 @@
 'use strict';
 
 var $       = require('jquery');
-var fitvids = require('fitvids');
+var fitVids = require('fitvids');
 var popup   = require('magnific-popup');
 
-console.log("================ Loading JS files for Ahmad Assaf Blog ============================");
+var blog = {
 
-// Add lightbox class to all image links
-$( `a[href$='.jpg'],
-    a[href$='.jpeg'],
-    a[href$='.JPG'],
-    a[href$='.png'],
-    a[href$='.gif']`
-).addClass("image-popup");
+    init: function() {
 
-// Add the fitvideo attribute for responsive videos
-$(function() { $(".content").fitVids() });
+        var blog = this;
 
-$(document).ready(function() {
-    $('.image-popup').magnificPopup({
-    type    : 'image',
-    tLoading: 'Loading image #%curr%...',
-    gallery : {
-      enabled           : true,
-      navigateByImgClick: true,
-      preload           : [0,1]
+        this.addImageLightBox();
+
+        $(document).ready(function() {
+            blog.addFitVideos();
+            blog.addMagnifiquePopup();
+            blog.addResponsiveMenu();
+        });
     },
-    image: {
-      tError      : '<a href="%url%">Image #%curr%</a> could not be loaded.',
-      },
-      removalDelay: 300,
-      mainClass   : 'mfp-fade'
-  });
-});
+
+    /**
+     * Add handlers to apply the responsive menu bar and handle the show/close of the menu itself
+     * @addResponsiveMenu
+     */
+
+    addResponsiveMenu: function() {
+
+        // Add the click listener to toggle the responsive menu class
+        $('.menu-toggle').on('click', function(){
+            $('.categories-list').toggleClass('responsive');
+        });
+
+        // This will make sure that we submit the form to add emails when we click anywhere outside of the email input field
+        $(document).mouseup(function(e) {
+
+            var container = $(".navigation-bar");
+
+            if (!container.is(e.target) // if the target of the click isn't the container...
+                && container.has(e.target).length === 0) // ... nor a descendant of the container
+            {
+                $('.categories-list').removeClass('responsive');
+            }
+        });
+    },
+
+    /**
+     * Add CSS class to all image links by checking the href of <a> tags and known image extension types
+     * the image-popup class is picked up by the magnific popup afterwards
+     * @addImageLightBox
+     */
+
+    addImageLightBox: function() {
+        return $(`a[href$='.jpg'],
+            a[href$='.jpeg'],
+            a[href$='.JPG'],
+            a[href$='.png'],
+            a[href$='.gif']`
+        ).addClass("image-popup");
+    },
+
+    /**
+     * lets your videos be responsive by keeping an intrinsic aspect ratio
+     * Reference: https://github.com/rosszurowski/vanilla-fitvids
+     *
+     * @addFitVideos
+     */
+
+    addFitVideos: function() {
+        return fitVids(".blog-post");
+
+    },
+
+    /**
+     * Fast, light and responsive lightbox plugin, for jQuery and Zepto.js
+     * Reference: http://dimsemenov.com/plugins/magnific-popup/documentation.html
+     *
+     * @addMagnifiquePopup
+     *
+     * @option {String} type    : The type of the popup galery
+     * @option {String} tLoading: Text that is displayed during loading. Can contain %curr% and %total% keys
+     * @option {Object} gallery : The gallery module allows you to switch the content of the popup and adds navigation arrows.
+     *                            It can switch and mix any types of content, not just images
+     *  @option {Boolean} enabled           : Enable gallery
+     *  @option {Boolean} navigateByImgClick: Flip through gallery by clicking on the image itself
+     *  @option {Array} preload             : Preloader in Magnific Popup is used as an indicator of current status.
+     *                                        If option enabled, it’s always present in DOM only text inside of it changes.
+     *                                        Below you can see explanation of CSS names that are applied to container that holds preloader
+     *                                        and content area depending on the state of current item
+     * @option {Object} image   :
+     *  @option {String} tError: Error message when image could not be loaded
+     * @option {Integer} removalDelay: Delay before popup is removed from DOM. Used for the animation
+     * @option {String} mainClass    : String that contains classes that will be added to the root element of popup wrapper and to dark overlay
+     */
+
+    addMagnifiquePopup: function() {
+        if (!!$('.image-popup').length) {
+            $('.image-popup').magnificPopup({
+                type    : 'image',
+                tLoading: 'Loading image #%curr%...',
+                gallery : {
+                  enabled           : true,
+                  navigateByImgClick: true,
+                  preload           : [0,1]
+                },
+                image: {
+                  tError      : '<a href="%url%">Image #%curr%</a> could not be loaded.',
+                },
+                removalDelay: 300,
+                mainClass   : 'mfp-fade'
+            });
+        }
+    }
+}
+
+blog.init();
--- gruntfile.js
+++ gruntfile.js
@@ -1,46 +1,104 @@
 module.exports = function(grunt) {
 
-  var pkg         = grunt.file.readJSON('package.json');
+    var pkg = grunt.file.readJSON('package.json');
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    clean: ['assets/js/build', '_site', 'assets/js/prod'],
+        clean: ['_site', '_site/assets/js/build/'],
 
-    mkdir: {
-      images: { options: { create: ['images/posts'] } },
-      js    : { options: { create: ['assets/js/prod'] } }
-    },
+        mkdir: {
+            images: {
+                options: {
+                    create: ['images/posts']
+                }
+            },
+            js: {
+                options: {
+                    create: ['assets/js/build']
+                }
+            }
+        },
 
-    copy: {
-      images: { files: [ {
-        expand: true,
-        cwd   : '_posts/images',
-        src   : ['**'],
-        dest  : 'images/posts/'
-      } ] }
-    },
+        copy: {
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: '_posts/images',
+                    src: ['**'],
+                    dest: 'images/posts/'
+                }]
+            }
+        },
 
-    browserify: {
-      dist: {
-        files: {'assets/js/prod/main.js' : ['assets/js/**/*.js']},
-        options: {
-          debug: true,
-          standalone: pkg['export-symbol']
+        browserify: {
+            dist: {
+                files: {
+                    '_site/assets/js/build/main.js': ['assets/js/**/*.js']
+                },
+                options: {
+                    debug: true,
+                    standalone: pkg['export-symbol']
+                }
+            }
+        },
+
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
+
+        bgShell: {
+            jekyllBuild: {
+                cmd: 'jekyll build',
+                done: function() {
+                    console.log("Finished Building Jekyll Site");
+                }
+            },
+            jekyllServe: {
+                cmd: 'jekyll serve'
+            }
+        },
+
+        watch: {
+            files: ['assets/js/**/*.js'],
+            tasks: ['browserify']
+        },
+
+        concurrent: {
+            serve: [
+                'mkdir:images', 'copy:images', 'bgShell:jekyllBuild', 'clean', 'browserify',
+                'watch',
+                'bgShell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
         }
-      }
-    }
 
+    });
 
-  });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-bg-shell');
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-exec');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Register the grunt build task
+    grunt.registerTask('build', ['mkdir:images', 'copy:images', 'bgShell:jekyllBuild', 'clean', 'browserify']);
 
-  grunt.registerTask('default', ['clean', 'mkdir:images', 'copy:images',  'mkdir:js' ,'browserify']);
+    // Register the grunt serve task
+    grunt.registerTask('serve', ['concurrent:serve']);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
 
 
 };
