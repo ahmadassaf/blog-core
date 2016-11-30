b38f50c8224cfd0ed0cbb97f5f21629ceae803a0
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-11-30 18:42:19 +0000

    removing gzipped contents

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,9 +2,3 @@
 set -x
 # Build the site with Jekyll
 grunt build
-
-#compress html/css/js files (-n removes the timestamp)
-find _site | egrep "\.(html|js|css)$" | xargs -I{} gzip -n -9 {}
-
-#remove the .gz extension
-find _site | egrep "\.(html|js|css)\.gz$" | xargs -I{} bash -c 'FILE="{}"; mv ${FILE} ${FILE%.*}'
