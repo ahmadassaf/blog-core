49ad108aedad4c533804010ab3dd1c81f629944f
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-11-30 18:55:54 +0000

    deploy travis deploy

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -x
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    cd _site
-    git init
 
-    git remote add deploy "ssh://hal@ahmadassaf.com:/var/www/blog"
-    git config user.name "Travis CI"
-    git config user.email "ahmad.a.assaf+travisCI@gmail.com"
+# Initialize a new git repo in _site, and push it to our server.
+cd _site
+git init
 
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-else
-    echo "Not deploying, since this branch isn't master."
-fi
+git remote add deploy "ssh://hal@ahmadassaf.com:/var/www/blog"
+git config user.name "Travis CI"
+git config user.email "ahmad.a.assaf+travisCI@gmail.com"
+
+git add .
+git commit -m "Deploy"
+git push --force deploy master
