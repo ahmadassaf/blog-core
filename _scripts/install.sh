c7aebcd9379913009a3cc1dcb74385caa84104e9
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-12-01 10:54:12 +0000

    trying to fix passphrase prompt

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_7bbee2d42c21_key -iv $encrypted_7bbee2d42c21_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
