0dd8ef2ad898428e6bc7fb5141fa7e857ba341f7
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-03-27 17:42:16 +0100

    disable remote link checks

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-timeout 30s bundle exec htmlproofer _site --url-ignore "/ahmadassaf.com|github.com/" --only-4xx --http-status-ignore "403" --check-html
+timeout 30s bundle exec htmlproofer _site --check-html
