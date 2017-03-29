c269c6789e64197f65167859c58775e8e9ddc940
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-03-29 23:11:39 +0100

    add exclusion for travis-ci.org in htmlproofer

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 
-bundle exec htmlproofer _site --url-ignore "/ahmadassaf.com|github.com/" --only-4xx --http-status-ignore "403" --check-html
+bundle exec htmlproofer _site --url-ignore "/ahmadassaf.com|github.com|travis-ci.org/" --only-4xx --http-status-ignore "403" --check-html
 
