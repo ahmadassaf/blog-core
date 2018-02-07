9943bad6efd16fbc69152e7854a265ab7b4ff0e3
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-02-07 09:28:00 +0000

    adding the new search bar but still needs styling arrangment

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -27,6 +27,45 @@ var blog = {
             blog.addSearchTrigger();
             blog.addHomePageScripts();
             blog.addStickyLogo();
+
+            var mainContainer = document.querySelector('.container__main'),
+            openCtrl = document.getElementById('btn-search'),
+            closeCtrl = document.getElementById('btn-search-close'),
+            searchContainer = document.querySelector('.search'),
+            inputSearch = searchContainer.querySelector('.search__input');
+    
+  
+    
+        function initEvents() {
+            console.log("init events ...");
+            openCtrl.addEventListener('click', openSearch);
+            closeCtrl.addEventListener('click', closeSearch);
+            document.addEventListener('keyup', function(ev) {
+                // escape key.
+                if( ev.keyCode == 27 ) {
+                    closeSearch();
+                }
+            });
+        }
+    
+        function openSearch() {
+            mainContainer.classList.add('container__main--overlay');
+            closeCtrl.classList.remove('btn--hidden');
+            searchContainer.classList.add('search--open');
+            setTimeout(function() {
+                inputSearch.focus();
+            }, 500);
+        }
+    
+        function closeSearch() {
+            mainContainer.classList.remove('container__main--overlay');
+            closeCtrl.classList.add('btn--hidden');
+            searchContainer.classList.remove('search--open');
+            inputSearch.blur();
+            inputSearch.value = '';
+        }
+    
+        initEvents();
         });
     },
     /**
