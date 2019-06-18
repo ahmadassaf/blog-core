93c22a2897178c931cd671326172d7df5b0d74a7
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2019-06-18 22:27:40 +0200

    Fixing the node-sass problem

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,7 +5,7 @@ set -x
 cd _site
 git init
 
-git remote add deploy "ssh://hal@104.225.220.86:8028/var/www/.build/blog"
+git remote add deploy "ssh://hal@104.225.220.86/var/www/.build/blog"
 git config user.name "Travis CI"
 git config user.email "ahmad.a.assaf+travisCI@gmail.com"
 
