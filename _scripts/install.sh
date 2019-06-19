56dd4177e243b1579e9cfc3d1012fc6108a5bbeb
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2019-06-19 11:15:36 +0200

    Updating the encryption keys

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_7bbee2d42c21_key -iv $encrypted_7bbee2d42c21_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_32ec88e6f89f_key -iv $encrypted_32ec88e6f89f_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
